df91209ffb3152a7a5eee73d30fa33ef01b600c8
turahul <37828973+turahul@users.noreply.github.com>
2020-04-11 22:50:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
+from flask import Flask
+app = Flask(__name__)
 
-from os import environ
-from FlaskWebProject import app
+
+@app.route('/' , methods = ["GET","POST"])
+def hello():
+    if request.method=="POST":
+        file = request.files["file"]
+        file.save(os.path.join("uploads",file.filename))
+        #filename = file.filename
+        image = Image.open(file)
+        model = tensorflow.keras.models.load_model('keras_model.h5')
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+        image_array = np.asarray(image)
+        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+        data[0] = normalized_image_array
+        prediction = model.predict(data)
+        print(prediction)
+        for x in prediction:
+            if x[0] > 0.50:
+              print("fake")
+              prediction ="Fake"
+            else:
+                print("real")
+                prediction="Real"
+        return render_template("WebPage1.html", message=prediction,)
+        return render_template("WebPage1.html", message=image)
+    """Renders the home page."""
+    return render_template("WebPage1.html", message="upload")
+    
+
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    app.run(host='127.0.0.1', port=8080, debug=True)
